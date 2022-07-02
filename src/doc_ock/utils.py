441cec6c56506a5001a7694d3f2c06d00583c1ad
José Pedro Silveira Martinez <jose.martinez@unico.io>
2022-07-02 19:00:59 +0000

    UPD:  allowing MethodType as well as FunctionType

--- src/doc_ock/utils.py
+++ src/doc_ock/utils.py
@@ -10,13 +10,13 @@ class InvalidArgumentsError(Exception):
 
 
 def validate_inputs(data_list, process, save_callback, num_procs, out_path, save_batch):
-    if isinstance(data_list, list)==False or len(data_list) == 0:
+    if not isinstance(data_list, list) or len(data_list) == 0:
         raise InvalidArgumentsError('data_list', 'list(str) > 0')
-    if isinstance(process, types.FunctionType) == False:
+    if not isinstance(process, (types.FunctionType, types.MethodType)):
         raise InvalidArgumentsError('process', 'function')
-    if isinstance(num_procs, int) == False or num_procs <= 0:
+    if not isinstance(num_procs, int) or num_procs <= 0:
         raise InvalidArgumentsError('num_procs', 'int > 0')
-    if isinstance(out_path, str) == False or len(out_path) == 0:
+    if not isinstance(out_path, str) or len(out_path) == 0:
         raise InvalidArgumentsError('out_path', 'len(str) > 0')
-    if isinstance(save_batch, int) == False or save_batch <= 0:
+    if not isinstance(save_batch, int) or save_batch <= 0:
         raise InvalidArgumentsError('save_batch', 'int > 0')
