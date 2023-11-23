6faa67b308b5313be9c87308ba0d4bf729d80841
Guilherme Fickel <guilhermefickel@gmail.com>
2023-11-23 20:58:06 -0300

    Feature/value output (#16)
    * FEAT: mp_lock_value function that runs mp_lock and returns the results
    
    * UPD: dummpy.py using mp_lock_value

--- src/doc_ock/mp_lock.py
+++ src/doc_ock/mp_lock.py
@@ -9,7 +9,7 @@ from multiprocessing import Pool, Lock
 from tqdm import tqdm
 import numpy as np
 
-from doc_ock.utils import validate_inputs
+from doc_ock.utils import validate_inputs, save_results, load_results
 
 
 def _discard_processed(data_list, out_path):
@@ -21,7 +21,8 @@ def _discard_processed(data_list, out_path):
     return list(set(data_list)-set(processed))
 
 def _proc_function(data_list, process, init_function, save_callback, out_path,
-                   save_batch, shared_data, ith_process, batch_processing=False):
+                   save_batch, shared_data, verbose, ith_process,
+                   batch_processing=False):
 
     def save(data_name, results):
         with lock:
@@ -37,11 +38,12 @@ def _proc_function(data_list, process, init_function, save_callback, out_path,
         if init_function is not None:
             init_vals = init_function(shared_data)
 
-        progress = tqdm(
-            total=len(data_list),
-            position=ith_process,
-            desc=f'Process #{ith_process}'
-        )
+        if verbose:
+            progress = tqdm(
+                total=len(data_list),
+                position=ith_process,
+                desc=f'Process #{ith_process}'
+            )
     
         # https://stackoverflow.com/questions/1154446/is-file-append-atomic-in-unix
         with open(f'{out_path}/user_output.txt', 'a') as fid:
@@ -58,7 +60,7 @@ def _proc_function(data_list, process, init_function, save_callback, out_path,
                         data_name, results = [], []
                         last_save = time.time()
 
-                    progress.update(1)
+                    if verbose: progress.update(1)
 
         if len(data_name) > 0:
             save(data_name, results)
@@ -73,7 +75,7 @@ def _init(l):
 
 def mp_lock(data_list: List[str], process: Callable, save_callback: Callable,
             num_procs: int, out_path: str, save_batch: int=10, shared_data: dict={},
-            init_function: Callable=None):
+            init_function: Callable=None, verbose: bool=True):
     """ Given a list of data and a process function, runs it in parallel and save
     the results to out_path. This function saves all the intermediate calculation,
     so you can always resume it.
@@ -113,10 +115,12 @@ def mp_lock(data_list: List[str], process: Callable, save_callback: Callable,
 
     final_data_list = _discard_processed(data_list, out_path)
     data_split = np.array_split(final_data_list, num_procs)
-    print(f'Data splitted in {len(data_split)} slices.')
+    if verbose:
+        print(f'Data splitted in {len(data_split)} slices.')
 
-    with tqdm(total=len(data_list)) as pbar:
-        args = [(data, process, init_function, save_callback, out_path, save_batch, shared_data, i) for i,data in enumerate(data_split)]
+    with tqdm(total=len(data_list), disable=not verbose) as pbar:
+        args = [(data, process, init_function, save_callback, out_path, save_batch,
+                 shared_data, verbose, i) for i,data in enumerate(data_split)]
         with Pool(processes=num_procs, initializer=_init, initargs=(lock,)) as pool:
             pool.starmap(_proc_function, args)
 
@@ -124,7 +128,7 @@ def mp_lock(data_list: List[str], process: Callable, save_callback: Callable,
 def mp_lock_batch(data_list: List[str], process: Callable,
                   save_callback: Callable, num_procs: int, out_path: str,
                   batch_size: int, save_batch: int=10, shared_data: dict={},
-                  init_function: Callable=None):
+                  init_function: Callable=None, verbose: bool=True):
     """ Given a list of data and a process function, runs it in parallel and save
     the results to out_path. This function saves all the intermediate calculation,
     so you can always resume it.
@@ -159,6 +163,8 @@ def mp_lock_batch(data_list: List[str], process: Callable,
     init_function : Callable
         Function that is called once, for every process. Receives shared_data and
         their return values are passed to the process function
+    verbose : bool
+        Controls tqdm output
     """
     validate_inputs(data_list, process, save_callback, num_procs, out_path, save_batch, batch_size)
     # This lock will be shared with all the processes
@@ -167,10 +173,68 @@ def mp_lock_batch(data_list: List[str], process: Callable,
     final_data_list = _discard_processed(data_list, out_path)
     group_data = [final_data_list[x:x+batch_size] for x in range(0, len(final_data_list), batch_size)]
     data_split = np.array_split(group_data, num_procs)
-    print(f'Data splitted in {len(data_split)}|{num_procs} slices of size {batch_size}')
+    if verbose:
+        print(f'Data splitted in {len(data_split)}|{num_procs} slices of size {batch_size}')
 
-    with tqdm(total=len(data_list)) as pbar:
-        args = [(data, process, init_function, save_callback, out_path, save_batch, shared_data, i, True) for i,data in enumerate(data_split)]
+    with tqdm(total=len(data_list), disable=not verbose) as pbar:
+        args = [(data, process, init_function, save_callback, out_path, save_batch,
+                 shared_data, verbose, i, True) for i,data in enumerate(data_split)]
 
         with Pool(processes=num_procs, initializer=_init, initargs=(lock,)) as pool:
             pool.starmap(_proc_function, args)
+
+
+
+def mp_lock_value(data_list: List[str], process: Callable, save_callback: Callable,
+            num_procs: int, out_path: str, save_batch: int=10, shared_data: dict={},
+            init_function: Callable=None, verbose: bool=True):
+    """ Given a list of data and a process function, runs it in parallel, save
+    the results to out_path, and when it finishes it returns the results. This
+    function saves all the intermediate calculation, so you can always resume it.
+
+    Parameters
+    ----------
+    data_list : list(str)
+        List of data to process. Can be a list of images, for example.
+    process : func
+        The processing function that gets an element from data_list, process it and
+        returns a (pickable) value
+    save_callback : func
+        Saving function callback that will receive the results from process. Must
+        be declared with the following arguments:
+        def save_callback(output_filepath, data_results, data_names)
+            output_filepath : str
+                Filename to save the results
+            data_results : list
+                A list of results from process function
+            data_names : list
+                A list of data names (instances from data_list)
+    num_procs : int
+        Number of processes to use
+    out_path : str
+        Output path
+    save_batch : int
+        Max number of results to group before saving
+    shared_data : dict
+        Data shared within all the processes
+    init_function : Callable
+        Function that is called once, for every process. Receives shared_data and
+        their return values are passed to the process function
+    verbose : bool
+        Controls tqdm output
+
+    Returns
+    -------
+    results_list : list()
+        A list of results generated by process, and matching data_list order
+    """
+    global my_save_callback
+    def my_save_callback(*kwargs):
+        save_results(*kwargs)
+        if save_callback is not None:
+            save_callback(*kwargs)
+
+    mp_lock(data_list, process, my_save_callback, num_procs, out_path,
+            save_batch, shared_data, init_function, verbose)
+
+    return load_results(data_list, out_path)
